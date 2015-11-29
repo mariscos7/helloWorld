5d7f0e2c7e0b875de6605f91929f8d1d2a307dfb
mariscos7 <mariscos7@tlen.pl>
2015-11-29 17:32:41 +0100

    inserting new lines

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,6 +3,12 @@ public class HelloWorld {
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
         System.out.println("Hello, World");
+        // Prints new line
+        System.out.println("\n");
+        // Prints space
+        System.out.println(" ");
+        // Prints "end."
+        System.out.println("end.");
     }
 
 }
